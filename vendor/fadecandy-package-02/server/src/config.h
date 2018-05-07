9f8de5f608611da9391fed24dc3d5c266e66287f
Shawn Morel <shawn@strangemonad.com>
2018-05-07 12:26:10 -0700

    update fadecandy

--- vendor/fadecandy-package-02/server/src/config.h
+++
@@ -1,7 +0,0 @@
-#ifndef TIMESPEC_TO_TIMEVAL
-#define TIMESPEC_TO_TIMEVAL(tv, ts)                                     \
-        do {                                                            \
-                (tv)->tv_sec = (ts)->tv_sec;                            \
-                (tv)->tv_usec = (ts)->tv_nsec / 1000;                   \
-        } while (0)
-#endif
